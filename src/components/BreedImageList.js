142a18e51c467818723be2a1dbef65c3ec1b36c6
user <allegrotech89@gmail.com>
2022-01-15 20:32:16 +0100

    aos timer updated to 500ms

--- src/components/BreedImageList.js
+++ src/components/BreedImageList.js
@@ -5,13 +5,13 @@ import "aos/dist/aos.css";
 function BreedImageList(props) {
 	useEffect(() => {
 		Aos.init({
-			duration: 2000,
+			duration: 500,
 		});
 	}, []);
 	const breedImage = props.exactDogBreed.map((Url, index) => {
 		return (
 			<div key={index} className=" image-items-Container">
-				<img data-aos="fadep-up" src={Url} alt="dog" />
+				<img data-aos="fade-left" src={Url} alt="dog" />
 			</div>
 		);
 	});
